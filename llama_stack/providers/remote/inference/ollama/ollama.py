5aced0361cd354210e7d940eed0a2ebe1aa4e74b
Eric Huang <erichuang@meta.com>
2025-02-03 12:14:53 -0800

    Support sys_prompt behavior in inference
    # What does this PR do?
    
    The current default system prompt for llama3.2 tends to overindex on tool calling and doesn't work well when the prompt does not require tool calling.
    
    This PR adds an option to override the default system prompt, and organizes tool-related configs into a new config object.
    
    - [ ] Addresses issue (#issue)
    
    
    ## Test Plan
    
    python -m unittest llama_stack.providers.tests.inference.test_prompt_adapter
    
    
    ## Sources
    
    Please link relevant resources if necessary.
    
    
    ## Before submitting
    
    - [ ] This PR fixes a typo or improves the docs (you can dismiss the other checks if that's the case).
    - [ ] Ran pre-commit to handle lint / formatting issues.
    - [ ] Read the [contributor guideline](https://github.com/meta-llama/llama-stack/blob/main/CONTRIBUTING.md),
          Pull Request section?
    - [ ] Updated relevant documentation.
    - [ ] Wrote necessary unit or integration tests.

--- llama_stack/providers/remote/inference/ollama/ollama.py
+++ llama_stack/providers/remote/inference/ollama/ollama.py
@@ -29,6 +29,7 @@ from llama_stack.apis.inference import (
     ResponseFormat,
     SamplingParams,
     ToolChoice,
+    ToolConfig,
     ToolDefinition,
     ToolPromptFormat,
 )
@@ -224,6 +225,7 @@ class OllamaInferenceAdapter(Inference, ModelsProtocolPrivate):
         tool_prompt_format: Optional[ToolPromptFormat] = None,
         stream: Optional[bool] = False,
         logprobs: Optional[LogProbConfig] = None,
+        tool_config: Optional[ToolConfig] = None,
     ) -> AsyncGenerator:
         model = await self.model_store.get_model(model_id)
         request = ChatCompletionRequest(
@@ -231,11 +233,10 @@ class OllamaInferenceAdapter(Inference, ModelsProtocolPrivate):
             messages=messages,
             sampling_params=sampling_params,
             tools=tools or [],
-            tool_choice=tool_choice,
-            tool_prompt_format=tool_prompt_format,
             stream=stream,
             logprobs=logprobs,
             response_format=response_format,
+            tool_config=tool_config,
         )
         if stream:
             return self._stream_chat_completion(request)
@@ -307,6 +308,7 @@ class OllamaInferenceAdapter(Inference, ModelsProtocolPrivate):
 
     async def _stream_chat_completion(self, request: ChatCompletionRequest) -> AsyncGenerator:
         params = await self._get_params(request)
+        print(params)
 
         async def _generate_and_convert_to_openai_compat():
             if "messages" in params:
