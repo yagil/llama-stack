c64b4a80b0cd903f14e12f57fecae3d51de0dea4
Neil Mehta <neil@lmstudio.ai>
2025-03-13 09:54:59 -0400

    sync 1

---
+++ llama_stack/providers/remote/inference/lmstudio/config.py
@@ -0,0 +1,11 @@
+from typing import Any, Dict
+from pydantic import BaseModel
+
+DEFAULT_LMSTUDIO_URL = "localhost:1234"
+
+class LmstudioImplConfig(BaseModel):
+    url: str = DEFAULT_LMSTUDIO_URL
+
+    @classmethod
+    def sample_run_config(cls, url: str = DEFAULT_LMSTUDIO_URL, **kwargs) -> Dict[str, Any]:
+        return {"url": url}
